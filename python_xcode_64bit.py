40a07bc3f7bd9db46adfac65da14a5057dcbe2b9
jtkaufman737 <jtkaufman737@gmail.com>
2016-11-27 12:54:57 -0500

    Fix for weird issue with Python versions doing tutsplus FlaskApp tutorial

---
+++ python_xcode_64bit.py
@@ -0,0 +1,9 @@
+https://hackercodex.com/guide/mac-osx-mavericks-10.9-configuration/
+
+https://hackercodex.com/guide/mac-osx-mavericks-10.9-configuration/
+
+https://hackercodex.com/guide/mac-osx-mavericks-10.9-configuration/
+
+#These were some of the steps I used when trying to make a Python/Flask app - it recommended I ensure my system defaults to 64 bit. Also weirdly in a hidden vim bash profile they
+#recommended I change there was what looked like a code snippet using rbenv dependencies for Python, which I found weird. Ultimately I got about halfway through and figured
+#I would keep the documentation because recently I've also had issues in Ruby with seemingly conflicting versions and I want to remember how I fixed it in case it comes up again
